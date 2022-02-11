30ae418274088a026489f31eaa2e3df880823401
kodencja <krzysztof.lalik.pl@gmail.com>
2022-02-11 23:01:14 +0100

    Correct styles in input components again

--- src/components/smallComponents/forForm/DateComp.jsx
+++ src/components/smallComponents/forForm/DateComp.jsx
@@ -4,57 +4,16 @@ import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import DesktopDatePicker from "@mui/lab/DesktopDatePicker";
 import { TextField } from "@mui/material";
 import useSomeStyles from "../../../styles/useSomeStyles";
-import { makeStyles } from "@mui/styles";
-import * as colors from "@mui/material/colors";
 
 const DateComp = (props) => {
-  // const { useStylesDateComp } = useSomeStyles();
-  // const classes = useStylesDateComp();
+  const { useStylesDateComp } = useSomeStyles();
+  const classes = useStylesDateComp();
 
   const {
     input: { name, onChange, onBlur, value, ...restInput },
     meta,
     ...rest
   } = props;
-
-  const useStyles = makeStyles({
-    root: {
-      marginTop: "10px",
-      marginBottom: "10px",
-      marginRight: "10px",
-      maxWidth: "200px",
-      "& .MuiInputBase-root": {
-        //   paddingLeft: 10,
-      },
-      // QUITE GOOD
-      // "& .MuiInput-underline.MuiInputBase-formControl.Mui-focused": {
-      "& .MuiInput-underline.Mui-focused": {
-        // backgroundColor: "burlywood",
-        //   backgroundColor: "#e8f0fe",
-        //   color: "darkslategray",
-      },
-      // "& .MuiInputBase-root-MuiOutlinedInput-root.Mui-error.Mui-error": {
-      // borderColor: "blue",
-      // color: "blue",
-      // width: 120,
-      // },
-    },
-    datePicker: {
-      "& .MuiCalendarPicker-root": {
-        // "& .MuiCalendarPicker-viewTransitionContainer": {
-        // backgroundColor: colors.grey[300],
-        backgroundColor: "coral",
-        // "& .MuiPaper-root": {
-        // backgroundColor: colors.grey[200] + " !important",
-
-        // bgcolor: "black !important",
-        color: colors.blue[500],
-        // },
-      },
-    },
-  });
-
-  const classes = useStyles();
 
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
