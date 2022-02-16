f9574604e4b7ea1ce40a165cb94c65f21e69565e
kodencja <krzysztof.lalik.pl@gmail.com>
2022-02-16 17:05:16 +0100

    Change jsonApi

--- src/constsNotInStore/jsonApi.js
+++ src/constsNotInStore/jsonApi.js
@@ -1,2 +1,2 @@
-export const JSON_API = "https://database-table-json-server-mui.herokuapp.com/api";
+export const JSON_API = "https://database-table-json-server-mui.herokuapp.com";
 // const JSON_API = "http://localhost:8000/grocery";
