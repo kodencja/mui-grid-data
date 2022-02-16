4ecb75529e2f1410a3cc2e5f023fa21dd4bed218
kodencja <krzysztof.lalik.pl@gmail.com>
2022-02-16 18:17:28 +0100

    Change server.js

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router('./data/db.json');
 const middlewares = jsonServer.defaults({
-    static: './app'
-    // static: './build'
+    static: './build'
 });
 const PORT = process.env.PORT || 8000;
 server.use(middlewares);
